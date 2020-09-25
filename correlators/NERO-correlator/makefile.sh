acf253bad92e0531fba6e333df21be3c31b1cdd0
kirbyherm <khermans21@gmail.com>
2020-09-25 18:01:47 -0400

    changing to WJ correlator2

--- correlators/NERO-correlator/makefile.sh
+++ correlators/NERO-correlator/makefile.sh
@@ -1,2 +1,3 @@
 g++ correlator.cpp $(root-config --cflags) $(root-config --glibs) -std=c++11 -O3 -o correlator
+g++ correlator2.cpp $(root-config --cflags) $(root-config --glibs) -std=c++11 -O3 -o correlator2
 
